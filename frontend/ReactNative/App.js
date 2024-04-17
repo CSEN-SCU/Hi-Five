33cf2d31a341d8ea18ffddc006b107410ac48f14
Brian Wiebe <51461287+wiebecoding@users.noreply.github.com>
2024-04-16 17:03:51 -0700

    Added login splash page UI.

--- frontend/ReactNative/App.js
+++ frontend/ReactNative/App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, ImageBackground, Pressable, StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={require('../../frontend/ReactNative/assets/concert.png')} resizeMode="cover"
+                             imageStyle={{opacity: 0.5}}>
+                <View style={styles.login}>
+                    <Text style={styles.nameTitle}>Hi-Five</Text>
+                    <Text style={styles.tagline}>Share the Vibe!</Text>
+                    <Pressable  style={styles.loginButton} onPress={()=>Alert.alert("You pressed the login button")}>
+                        <Icon name='spotify' size={20} style={styles.iconStyle}/>
+                        <Text style={styles.loginText}>Sign In With Spotify</Text>
+                    </Pressable>
+                </View>
+            </ImageBackground>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#000',
+    },
+    iconStyle:{
+        justifyContent: "center",
+        paddingRight: 10,
+        paddingVertical: 2,
+    },
+    loginButton:{
+        backgroundColor:"#1ED760",
+        borderRadius: 4,
+        alignContent: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        display: "inline-block",
+        marginHorizontal: "20%"
+},
+    loginText: {
+        fontSize: 20,
+        color: "#000",
+        textAlign: 'center',
+        fontWeight: 'bold',
+
+    },
+    nameTitle: {
+        color: '#cfd',
+        fontSize: 75,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    tagline: {
+        color: '#fff',
+        fontSize: 18,
+        textAlign: 'center',
+        marginBottom: 200,
+    },
+    login: {
+        height: '100%',
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
 });
