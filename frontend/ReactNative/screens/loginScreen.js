a5add20c6906abbcc5cc06c2f9c315c7090d7338
hilllls <hilaryle04@gmail.com>
2024-04-22 16:41:55 -0700

    create blur & change fonts

--- frontend/ReactNative/screens/loginScreen.js
+++ frontend/ReactNative/screens/loginScreen.js
@@ -1,16 +1,28 @@
 import {Alert, ImageBackground, Pressable, StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { useFonts, Poppins_700Bold, Poppins_400Regular } from '@expo-google-fonts/poppins';
+
 
 const LoginScreen = () => {
+
+    let [fontsLoaded] = useFonts({
+        Poppins_700Bold,
+        Poppins_400Regular
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (<View style={styles.container}>
             <ImageBackground source={require('../../../frontend/ReactNative/assets/concert.png')} resizeMode="cover"
-                             imageStyle={{opacity: 0.5}}>
+            imageStyle={{ opacity: 0.5 }} blurRadius={3}>
                 <View style={styles.login}>
                     <Text style={styles.nameTitle}>Hi-Five</Text>
                     <Text style={styles.tagline}>Share the Vibe!</Text>
                     <Pressable  style={styles.loginButton} onPress={()=>Alert.alert("You pressed the login button")}>
                         <Icon name='spotify' size={20} style={styles.iconStyle}/>
-                        <Text style={styles.loginText}>Sign In With Spotify</Text>
+                        <Text style={styles.loginText}>Login with Spotify</Text>
                     </Pressable>
                 </View>
             </ImageBackground>
@@ -47,19 +59,19 @@ const styles = StyleSheet.create({
         color: "#000",
         textAlign: 'center',
         fontWeight: 'bold',
-
     },
     nameTitle: {
         color: '#cfd',
         fontSize: 75,
         textAlign: 'center',
-        fontWeight: 'bold'
+        fontFamily: 'Poppins_700Bold'
     },
     tagline: {
         color: '#fff',
         fontSize: 18,
         textAlign: 'center',
         marginBottom: 200,
+        fontFamily: 'Poppins_400Regular',
     },
     login: {
         height: '100%',
