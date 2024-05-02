23f5520acbd78b391d790e02254ada7af7517365
hilllls <hilaryle04@gmail.com>
2024-05-01 17:03:46 -0700

    get files

--- frontend/ReactNative/App.js
+++
@@ -1,9 +0,0 @@
-import Feed from "./screens/feed";
-import LoginScreen from "./screens/loginScreen";
-
-export default function App() {
-    return (
-        // <LoginScreen />
-        <Feed />
-    );
-}
