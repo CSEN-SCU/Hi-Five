270ac28fef09ecab19ffd28d2c76a5158fcf0ff5
hilllls <hilaryle04@gmail.com>
2024-05-02 14:50:04 -0700

    edit post

--- frontend/ReactNative/screens/postItem.js
+++ frontend/ReactNative/screens/postItem.js
@@ -1,69 +1,62 @@
+import React from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { useFonts, Poppins_700Bold, Poppins_400Regular } from '@expo-google-fonts/poppins';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const PostItem = () => {
-
-    let [fontsLoaded] = useFonts({
-        Poppins_700Bold,
-        Poppins_400Regular
-    });
-
-    if (!fontsLoaded) {
-        return null;
+export default class PostItem extends React.Component {
+    state = {
+        buttonColor: '#FFFFFF'
     }
 
-    return (
-        <View>
-            <View style={styles.user_info}>
-                <Image
-                    style={styles.profile_pic}  // required Dimensions and styling of Image
-                    source={require('../../../frontend/ReactNative/assets/concert.png')} // enter your avatar image path 
-                />
-                <Text style={styles.username}>johnjohn</Text>
-            </View>
-            <View style={styles.card}>
-                <Image
-                    style={styles.song_cover}  // required Dimensions and styling of Image
-                    source={require('../../../frontend/ReactNative/assets/heros-cover.png')} // enter your avatar image path 
-                />
-                <Text style={styles.song_title}>Superhero</Text>
-                <Text style={styles.song_artist}>Metro Boomin, Future, Chris Brown</Text>
+    handleClick = () => {
+        // Toggle between white and red color
+        const newColor = this.state.buttonColor === '#FFFFFF' ? '#B2EED3' : '#FFFFFF';
+        this.setState({ buttonColor: newColor });
 
-                {/* slider / player */}
-                {/* <View style={{ margin: 5 }}>
-                    <Slider
-                        minimumValue={0}
-                        maximumValue={this.state.trackLength}
-                        trackStyle={styles.track}
-                        thumbStyle={styles.thumb}
-                        minimumTrackTintColor='#FFFFFF'
-                        onValueChange={seconds => this.changeTime(seconds)}
-                    ></Slider>
-                    <View style={{ marginTop: -12, flexDirection: "row", justifyContent: "space-between" }}>
-                        <Text style={styles.timeStamp}>{this.state.timeElapsed}</Text>
-                        <Text style={styles.timeStamp}>{this.state.timeRemaining}</Text>
-                    </View>
-                </View> */}
+        if (newColor == '#FFFFFF') {
+            console.log('Hi-Five button pressed (white)');
+        }
+        else {
+            console.log('Hi-Five button pressed (teal)');
+        }
+    };
 
-                <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginTop: 16 }}>
-                    <TouchableOpacity onPress={() => console.log('Close button pressed')}>
-                        <Icon name='close' size={43} color='#FFFFFF' />
-                    </TouchableOpacity >
-                    <TouchableOpacity style={styles.playButtonContainer} onPress={() => console.log('Play button pressed')}>
-                        <Icon name='play' size={50} color='#FFFFFF' />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => console.log('Hi-Five button pressed')}>
-                        <Icon name='hand-right' size={30} color='#FFFFFF' />
-                    </TouchableOpacity>
+    render() {
+        return (
+            <View>
+                <View style={styles.user_info}>
+                    <Image
+                        style={styles.profile_pic}  // required Dimensions and styling of Image
+                        source={require('../../../frontend/ReactNative/assets/concert.png')} // enter your avatar image path 
+                    />
+                    <Text style={styles.username}>johnjohn</Text>
                 </View>
+                <View style={styles.card}>
+                    <Image
+                        style={styles.song_cover}  // required Dimensions and styling of Image
+                        source={require('../../../frontend/ReactNative/assets/heros-cover.png')} // enter your avatar image path 
+                    />
+                    <Text style={styles.song_title}>Superhero</Text>
+                    <Text style={styles.song_artist}>Metro Boomin, Future, Chris Brown</Text>
+
+
+                    <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginTop: 30 }}>
+                        <TouchableOpacity onPress={() => console.log('Close button pressed')}>
+                            <Icon name='close' size={40} color='#FFFFFF' />
+                        </TouchableOpacity >
+                        <TouchableOpacity style={styles.playButtonContainer}>
+                            <Icon name='play' size={50} color='#FFFFFF' />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this.handleClick}>
+                            <Icon name='hand-right' size={30} color={this.state.buttonColor} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
             </View>
-
-        </View>
-    )
+        );
+    }
 }
-
-export default PostItem;
 
 const styles = StyleSheet.create({
     card: {
@@ -73,7 +66,7 @@ const styles = StyleSheet.create({
         paddingVertical: 25,
         paddingHorizontal: 35,
         width: 340,
-        height: 475,
+        height: 450,
         borderRadius: 15,
         marginBottom: 15
     },
@@ -113,24 +106,6 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: '#FFFFFF'
     },
-
-    // track: {
-    //     height: 2,
-    //     borderRadius: 1,
-    //     backgroundColor: '#777777'
-    // },
-
-    // thumb: {
-    //     width: 8,
-    //     height: 8,
-    //     backgroundColor: "#FFFFFF"
-    // },
-
-    // timeStamp: {
-    //     fontSize: 11,
-    //     fontWeight: "500",
-    //     color: "#FFFFFF"
-    // },
 
     playButtonContainer: {
         alignItems: "center",
