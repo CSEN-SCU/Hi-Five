c85c8400d52d35bce8cbcc5a67ff39989089a7f4
Matty Herzig <mattyherzig@yahoo.com>
2024-05-02 15:50:51 -0700

    consistent naming

--- backend/SpotifyAPI/authentication.js
+++ backend/SpotifyAPI/authentication.js
@@ -18,7 +18,7 @@ let global_user_id = "none currently";
 
 const spotifyAuthAPI = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.SECRET_KEY,
+  clientSecret: process.env.CLIENT_SECRET,
   redirectUri: process.env.REDIRECT_URI,
 });
 
