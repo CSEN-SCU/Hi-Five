2533f6b68c703d1313c7ea47d719432f13469432
Brian Wiebe <51461287+wiebecoding@users.noreply.github.com>
2024-05-07 11:37:33 -0700

    Profile Screen Fix

--- frontend/ReactNative/screens/profileScreen.js
+++ frontend/ReactNative/screens/profileScreen.js
@@ -1,12 +1,48 @@
 import {Alert, Image, Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import React from 'react';
 
-const ProfileScreen = ({ navigation }) => {
+const ImageCount = props => {
+    return (
+        <View style={styles.countImageCont}>
+            <Image style={styles.photo} source={require('../assets/cover.jpg')}/>
+            <Text style={styles.historyCount}>{props.number}</Text>
+        </View>
+
+    );
+}
+const HistoryRowTop = () => {
+    return (
+        <View style={styles.rowContainer}>
+            <ImageCount number = "1"/>
+            <ImageCount number = "2"/>
+            <ImageCount number = "3"/>
+            <ImageCount number = "4"/>
+            <ImageCount number = "5"/>
+            <ImageCount number = "6"/>
+            <ImageCount number = "7"/>
+        </View>
+    );
+};
+const HistoryRowButtom = () => {
+    return (
+        <View style={styles.rowContainer}>
+            <ImageCount number = "8"/>
+            <ImageCount number = "9"/>
+            <ImageCount number = "10"/>
+            <ImageCount number = "11"/>
+            <ImageCount number = "12"/>
+            <ImageCount number = "13"/>
+            <ImageCount number = "14"/>
+        </View>
+    );
+};
+const ProfileScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             {/*Top Nav Bar*/}
             <View style={styles.topBar}>
-                <Pressable onPress={onPress = () => navigation.goBack()}>
+                <Pressable onPress={()=>Alert.alert("You pressed the back button")}>
                     <Icon name='arrow-left' size={20} style={styles.iconTopStyle}/>
                 </Pressable>
                 <Text style={styles.navTitle}>Profile</Text>
@@ -33,9 +69,8 @@ const ProfileScreen = ({ navigation }) => {
             </View>
             <View style={styles.historyContainer}>
                 <Text style={styles.historyTitle}>14 Days Ago</Text>
-                <Text style={styles.historyTitle}>Placeholder</Text>
-                <Text style={styles.historyTitle}>Placeholder</Text>
-                <Text style={styles.historyTitle}>Placeholder</Text>
+                <HistoryRowTop/>
+                <HistoryRowButtom/>
             </View>
         </SafeAreaView>
     )
@@ -55,6 +90,7 @@ const styles = StyleSheet.create({
         paddingVertical: 2,
         color: '#B2EED3'
     },
+
     topBar:{
         marginTop: 10,
         flexDirection: 'row',
@@ -97,6 +133,12 @@ const styles = StyleSheet.create({
         paddingVertical: 3,
         color: '#fff'
     },
+    countImageCont:{
+        flexWrap:"nowrap",
+        alignItems: 'center',
+        justifyContent: "center",
+        flexDirection: 'column',
+    },
     nameText: {
         marginTop: 35,
         fontSize: 35,
@@ -138,12 +180,31 @@ const styles = StyleSheet.create({
         marginTop: 50,
         backgroundColor: '#2E2C2C',
         marginHorizontal: 20,
-        borderRadius: 20
+        borderRadius: 20,
+        paddingBottom: 20,
     },
     historyTitle:{
         color: "#fff",
         paddingLeft: 20,
-        paddingVertical: 10,
-        fontSize: 16,
+        paddingTop: 10,
+        paddingBottom: 10,
+        fontSize: 20,
+    },
+    rowContainer:{
+        //marginLeft: 20,
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: "center",
+    },
+    photo:{
+        marginTop: 10,
+        marginHorizontal: 5,
+        opacity: 60,
+        width: 40,
+        height: 40,
+        overflow: "hidden",
+    },
+    historyCount:{
+        color: "#fff"
     },
 });
