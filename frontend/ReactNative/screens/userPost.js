e6aafdffdba54459b774c955f47c5c1ad2febff2
hilllls <hilaryle04@gmail.com>
2024-05-17 12:45:42 -0700

    Update userPost.js

--- frontend/ReactNative/screens/userPost.js
+++ frontend/ReactNative/screens/userPost.js
@@ -1,7 +1,11 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, State } from "react-native";
 import { useFonts, Poppins_700Bold, Poppins_400Regular } from '@expo-google-fonts/poppins';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const UserPost = () => {
+
+    const [posted, setPosted] = useState(false);
 
     let [fontsLoaded] = useFonts({
         Poppins_700Bold,
@@ -13,17 +17,24 @@ const UserPost = () => {
     }
 
     return (
-
-        // clickable ??
-
-        <View style={styles.card}>
-            <Image
-                style={styles.song_cover}
-                source={require('../../../frontend/ReactNative/assets/heros-cover.png')}
-            />
+        <View style={[styles.card, posted ? styles.postedCard : null]}>
+            {posted ? (
+                <Image
+                    style={styles.song_cover}
+                    source={require('../../../frontend/ReactNative/assets/heros-cover.png')}
+                />
+            ) : (
+                <View style={styles.music_icon_container}>
+                    <Icon name="musical-notes-outline" size={32.5} color="#B2EED3"/>
+                </View>
+            )}
             <View style={{ flexDirection: 'column' }}>
-                <Text style={styles.song_title}>Superhero</Text>
-                <Text style={styles.song_artist}>Metro Boomin, Future, Chris Brown</Text>
+                <Text style={styles.song_title}>
+                    {posted ? "Superhero" : "Add a song"}
+                </Text>
+                <Text style={styles.song_artist}>
+                    {posted ? "Metro Boomin, Future, Chris Brown" : "Share the vibe!"}
+                </Text>
             </View>
         </View>
     )
@@ -80,5 +91,15 @@ const styles = StyleSheet.create({
     song_title: {
         fontSize: 15,
         color: '#FFFFFF'
+    },
+
+    music_icon_container: {
+        backgroundColor: '#000000',
+        borderRadius: 10,
+        height: 50,
+        width: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 10
     }
 });
