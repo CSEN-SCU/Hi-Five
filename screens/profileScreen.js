11585635bce704358480fe9e5a28baed84fcaed7
srivatsa311 <150607914+VatsaPuranam@users.noreply.github.com>
2024-06-05 14:11:39 -0700

    Update profileScreen.js

--- screens/profileScreen.js
+++ screens/profileScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Alert, Image, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getUserAccessToken } from '../backend/SpotifyAPI/auth'; // Import getUserAccessToken function
 import { getUserDisplayNameUsingAccessToken } from '../backend/SpotifyAPI/auth'; // Import getUserDisplayNameUsingAccessToken function
 
